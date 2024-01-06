966be857ec217e679f47a221de3dad69bd309150
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2024-01-06 12:41:57 +0100

    error page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,9 +1,14 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError
+} from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 
 const ErrorPage = () => {
   const error = useRouteError()
   isRouteErrorResponse(error)
+  const navigate = useNavigate()
 
   return (
     <>
@@ -15,6 +20,9 @@ const ErrorPage = () => {
             ? 'This page does not exist.'
             : 'An unexpected error occurred.'}
         </p>
+        <button className='btn-secondary btn' onClick={() => navigate('/')}>
+          Back Home
+        </button>
       </div>
     </>
   )
