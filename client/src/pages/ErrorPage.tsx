2ee70946a59e9ad0ab9d79300ee8ce1abe7c71c2
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2024-01-06 14:23:15 +0100

    update error

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,31 +1,27 @@
-import {
-  isRouteErrorResponse,
-  useNavigate,
-  useRouteError
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
-import Button from '../components/ui/Button'
 
 const ErrorPage = () => {
   const error = useRouteError()
-  isRouteErrorResponse(error)
-  const navigate = useNavigate()
+  const isErrorResponse = isRouteErrorResponse(error)
 
   return (
     <>
       <Navbar />
-      <div className='flex flex-col items-center justify-center h-screen space-y-4'>
+      <div className='flex flex-col items-center justify-center space-y-4'>
         <h1 className='text-7xl'>Oops...</h1>
         <p className='text-lg'>
-          {isRouteErrorResponse(error)
+          {isErrorResponse
             ? 'This page does not exist.'
             : 'An unexpected error occurred.'}
         </p>
-        <Button className='btn-error' onClick={() => navigate('/')}>
+        <Link to='/' className='btn-error btn'>
           Back Home
-        </Button>
+        </Link>
       </div>
     </>
   )
 }
+
 export default ErrorPage
