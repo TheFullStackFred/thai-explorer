843c3202812b611018d536108967efc5aa2f6572
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2024-01-07 18:18:41 +0100

    update nav

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -3,14 +3,14 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 
 const Navbar = () => {
   return (
-    <nav className='mb-8 shadow-lg navbar'>
-      <div className='container mx-auto'>
-        <div className='flex-1 text-nowrap'>
-          <Link to='/' className='text-lg font-bold'>
+    <nav className='p-6 mb-10 shadow-lg navbar'>
+      <div className='flex items-center justify-between w-full'>
+        <div>
+          <Link to='/' className='text-lg font-bold text-nowrap'>
             Thai Explorer
           </Link>
         </div>
-        <div className='hidden xs:flex'>
+        <div className='hidden sm:flex'>
           <Link to='/' className='btn-ghost btn btn-sm rounded-btn'>
             Home
           </Link>
@@ -20,12 +20,12 @@ const Navbar = () => {
         </div>
       </div>
       <div className='dropdown dropdown-end'>
-        <div tabIndex={0} role='button' className='btn btn-ghost xs:hidden'>
+        <div tabIndex={0} role='button' className='btn btn-ghost sm:hidden'>
           <RxHamburgerMenu size={25} />
         </div>
         <ul
           tabIndex={0}
-          className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+          className='menu menu-sm dropdown-content  z-[1] shadow-lg bg-base-100 rounded-box w-52 border-2 border-solid'
         >
           <Link to='/' className='text-white btn-ghost btn btn-sm rounded-btn'>
             Home
